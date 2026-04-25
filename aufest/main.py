0abdeb6894eb59eae0f466ab2e8d5188121e4b2e
codes <277353706+codes-and-stuffs@users.noreply.github.com>
2026-04-25 18:22:33 +0100

    moved over matching algorithm stuff

--- aufest/main.py
+++ aufest/main.py
@@ -1,4 +1,5 @@
 # real basic ass skeleton code, please feel free to change it up as you see fit
+from graph_stuff import HKGraph
 
 def get_pitches(filename):
     pass
@@ -33,11 +34,31 @@ def create_graph(artists=dict, pitches=dict):
     return edge_list
 
 
-def bipartite_match(edge_list):
+def bipartite_match(artists, pitches, edge_list):
     # output: pairs, unmatched artists, unmatched pitches
-    
-    # i think here it'd be easiest to just import the graph stuff script and run it all there 
-    # it just needs an input of the edge list and the number of artists + authors!
+
+    # shuffle list of edges
+    print("ORIGINAL LIST:")
+    print(hardcoded_edges)
+    random.shuffle(hardcoded_edges)
+    print("SHUFFLED LIST:")
+    print(hardcoded_edges)
+
+    # define graph size and then make it
+    number_of_pitches = len(pitches) # authors
+    number_of_artists = len(artists) # artists
+    graph = HKGraph(number_of_pitches, number_of_artists)
+
+    # add edges one by one, checking they're within the correct range
+    for u, v in edges_data:
+        print(f"  Adding edge: ({u}, {v})")
+        if 1 <= u <= number_of_pitches and 1 <= v <= number_of_artists:
+            g.add_edge(u, v)
+        else:
+            print(f"Warning: Skipping invalid edge ({u}, {v}) - indices out of range [1..{number_of_pitches}] or [1..{number_of_artists}]")
+
+    # run the algorithm (this gives the size, list output printed from inside but i should change that)
+    max_matching_size = graph.hopcroft_karp_algorithm()
     
     pass
 
@@ -46,5 +67,5 @@ def main():
     pitches = get_pitches()
     artists = get_artists()
     edge_list = create_graph()
-    matched_pairs, unmatched_artists, unmatched_pitches = bipartite_match(edge_list)
+    matched_pairs, unmatched_artists, unmatched_pitches = bipartite_match(artists, pitches, edge_list)
     # then pretty print the outputs
