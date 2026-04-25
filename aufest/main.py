e13ebb090b4cd715afc3b971cd151ac629245cc6
codes <277353706+codes-and-stuffs@users.noreply.github.com>
2026-04-25 18:05:37 +0100

    Update main.py
    added the edge validation here!

--- aufest/main.py
+++ aufest/main.py
@@ -9,19 +9,42 @@ def get_artists(filename):
 
 
 def create_graph(artists=dict, pitches=dict):
-    # output: graph
-    pass
+    # output: list of graph edges
+    edge_list = []
+
+    for artistID in artists:
+        print(artists[artistID]["preferences"])
+        for pitchID in pitches:
+            print(pitches[pitchID]["pitchID"])
+            if (pitches[pitchID]["pitchID"] in artists[artistID]["preferences"]) or (pitches[pitchID]["fandom"] in artists[artistID]["wildcards"]):
+                if (pitches[pitchID]["adults_only"] == "true") and (artists[artistID]["adult"] == "false"):
+                    print("age mismatch")
+                elif len(list(set(pitches[pitchID]["mediums"]) & set(artists[artistID]["mediums"]))) == 0:
+                    print("medium mismatch")
+                elif (pitches[pitchID]["discord"] in artists[artistID]["dnms"]) \
+                    or (artists[artistID]["discord"] in pitches[pitchID]["dnms"]):
+                    print("dnm mismatch")
+                elif (pitches[pitchID]["discord"]) == artists[artistID]["discord"]:
+                    print("same participant mismatch")
+                else:
+                    print("ok")
+                    edge_list.append((pitchID, artistID))
+    print(edge_list)
+    return edge_list
 
 
-def bipartite_match(graph):
+def bipartite_match(edge_list):
     # output: pairs, unmatched artists, unmatched pitches
+    
+    # i think here it'd be easiest to just import the graph stuff script and run it all there 
+    # it just needs an input of the edge list and the number of artists + authors!
+    
     pass
 
 
 def main():
     pitches = get_pitches()
     artists = get_artists()
-    graph = create_graph()
-    matched_pairs, unmatched_artists, unmatched_pitches = bipartite_match(graph)
+    edge_list = create_graph()
+    matched_pairs, unmatched_artists, unmatched_pitches = bipartite_match(edge_list)
     # then pretty print the outputs
-
