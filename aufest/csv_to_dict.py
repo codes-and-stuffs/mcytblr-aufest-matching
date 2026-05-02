230af93d99a709b3117c8b1f7dca7aab740294c2
codes <277353706+codes-and-stuffs@users.noreply.github.com>
2026-05-02 14:57:02 +0100

    Update csv_to_dict.py comments to be cleaner

--- aufest/csv_to_dict.py
+++ aufest/csv_to_dict.py
@@ -1,9 +1,7 @@
-# imports
-import csv # see https://docs.python.org/3/library/csv.html
+import csv # perfect library for our uses! see https://docs.python.org/3/library/csv.html
 
 # these functions load from csv files into new dictionaries
 def load_authors(name_of_author_csv):
-    # UNTESTED
     author_dict = {}
     with open(name_of_author_csv, newline='') as signup_csv:
         signup_reader = csv.DictReader(signup_csv)
@@ -14,7 +12,6 @@ def load_authors(name_of_author_csv):
     return author_dict
 
 def load_artists(name_of_artist_csv):
-    # UNTESTED
     artist_dict = {}
     with open(name_of_artist_csv, newline='') as signup_csv:
         signup_reader = csv.DictReader(signup_csv)
@@ -24,28 +21,30 @@ def load_artists(name_of_artist_csv):
             next_id += 1
     return artist_dict
 
+# does NOT RUN in normal execution - use this for debugging
 if __name__ == "__main__":
     # files to use
-    name_of_author_csv = ""
-    name_of_artist_csv = ""
+    name_of_author_csv = "pitches.csv"
+    name_of_artist_csv = "artists.csv"
 
-    # when real csvs added, we can uncomment this
+    # unload data from csvs
     author_dict = load_authors(name_of_author_csv)
     artist_dict = load_artists(name_of_artist_csv)
     
-    # but for now test dictionaries used here
-    author_dict = {
-        1: {"pitchID": "D01", "discord": "tubbo_", "fandom": "dream smp", "adults_only": "false", "mediums": ["art", "podfic"], "dnms": []},
-        2: {"pitchID": "E01", "discord": "falsetwo", "fandom": "empires smp", "adults_only": "true", "mediums": ["webweave"], "dnms": []},
-        3: {"pitchID": "D02", "discord": "someone_else", "fandom": "dream smp", "adults_only": "false", "mediums": ["art","webweave", "podfic", "other"], "dnms": ["tubbo_"]}
-    }
+    # old test dictionaries - uncomment if you want to skip the file loading stage
+    # author_dict = {
+    #     1: {"pitchID": "D01", "discord": "tubbo_", "fandom": "dream smp", "adults_only": "false", "mediums": ["art", "podfic"], "dnms": []},
+    #     2: {"pitchID": "E01", "discord": "falsetwo", "fandom": "empires smp", "adults_only": "true", "mediums": ["webweave"], "dnms": []},
+    #     3: {"pitchID": "D02", "discord": "someone_else", "fandom": "dream smp", "adults_only": "false", "mediums": ["art","webweave", "podfic", "other"], "dnms": ["tubbo_"]}
+    # }
     
-    artist_dict = {
-        1: {"discord": "aaaaaaa", "preferences": ["D01","D03"], "wildcards": ["dream smp"], "adult": "false", "mediums": ["art"], "dnms": []},
-        2: {"discord": "skdfsdjkftghnfghh", "preferences": ["E01"], "wildcards": [], "adult": "true", "mediums": ["art", "webweave"], "dnms": []},
-        3: {"discord": "awawsawawawawa", "preferences": ["D01"], "wildcards": ["vampires smp"], "adult": "true", "mediums": ["webweave", "podfic"], "dnms": []}
-    }
-    
+    # artist_dict = {
+    #     1: {"discord": "aaaaaaa", "preferences": ["D01","D03"], "wildcards": ["dream smp"], "adult": "false", "mediums": ["art"], "dnms": []},
+    #     2: {"discord": "skdfsdjkftghnfghh", "preferences": ["E01"], "wildcards": [], "adult": "true", "mediums": ["art", "webweave"], "dnms": []},
+    #     3: {"discord": "awawsawawawawa", "preferences": ["D01"], "wildcards": ["vampires smp"], "adult": "true", "mediums": ["webweave", "podfic"], "dnms": []}
+    # }
+
+    # print loaded data
     print("Author dictionary:")
     print(author_dict)
     print("Artist dictionary:")
